9bc3b2dd9bb9149f1c9ac325d75cc380db3c2f64
James Allen <jamesallen74@gmail.com>
2020-09-03 21:54:39 -0400

    Init Commit

---
+++ Series1-BookList-MVC-WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Series1_BookList_MVC_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
